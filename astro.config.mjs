665234f3d0b6c8288b859f586500eadb268858fa
SaulDT <saul.domingueztorres.21@gmail.com>
2026-03-28 23:52:13 -0600

    problema de push

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,9 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://Saul-MX.github.io',
-  base: '/Libreria-Valeria/',
+  base: '/Libreria-Valeria',
   vite: {
     plugins: [tailwindcss()],
   },
